4356f8173ade31b738b27e1a79f84348b8af8617
JosueJurgen <94497675+JosueJurgen@users.noreply.github.com>
2022-08-29 11:40:56 -0500

    Add files via upload

---
+++ clientWindows_tcp.c
@@ -0,0 +1,89 @@
+// Nombre : Cliente
+// Proposito : Envia un mensaje y muestra el mensaje
+ devuelto por el servidor
+// Autor : Edwin Carrasco (Adaptado de [1])
+// FCreacion : --
+// FModific. : --
+// Compilacion : gcc -o Cliente Cliente.c
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#define BUFFSIZE 32 
+
+void AvisarError(char *mensaje)
+{
+ perror(mensaje);
+ exit(1);
+}
+int main(int argc, char *argv[])
+{
+ // Variables
+ int sock;
+ struct sockaddr_in echoserver;
+ char buffer[BUFFSIZE];
+ unsigned int echolen;
+ int received = 0;
+
+ // Verificar argumentos necesarios para correr programa
+ if (argc != 4)
+ {
+ fprintf(stderr, "USO: Cliente <ip de servidor>
+ <mensaje> <puerto>\n");
+ exit(1);
+ }
+
+ // Crear el socket TCP
+ if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP))< 0)
+ {
+ AvisarError("No se pudo crear socket");
+ }
+
+ /* Construir la estructura sockaddr_in del servidor */
+ // Limpiar la estructura
+ memset(&echoserver, 0, sizeof(echoserver));
+ // Protocolo de Internet/IP
+ echoserver.sin_family = AF_INET;
+ // Direccion IP
+ echoserver.sin_addr.s_addr = inet_addr(argv[1]);
+ // Puerto del servidor
+ echoserver.sin_port=htons(atoi(argv[3]));
+ // Establecer la conexion
+ if (connect(sock,(struct sockaddr *) &echoserver,
+ sizeof(echoserver)) < 0 )
+ {
+ AvisarError("No se pudo conectar al servidor");
+ }
+
+ // Enviar en mensaje al servidor
+ echolen = strlen(argv[2]);
+if (send(sock, argv[2], echolen, 0) != echolen)
+ {
+ AvisarError("Inconsistencia en el numero de bytes
+ enviados");
+ }
+
+ // Recibir el mensaje de vuelta desde el servidor
+ fprintf(stdout, "Recibido: ");
+ while (received < echolen)
+ {
+ int bytes = 0;
+ if ((bytes = recv(sock, buffer, BUFFSIZE-1, 0)) < 1)
+ {
+ AvisarError("No se pudo recibir bytes desde el
+ servidor");
+ }
+
+ received += bytes;
+ buffer[bytes]='\0'; // Cerciorarse que la cadena
+ termine en \0
+ fprintf(stdout, buffer);
+ }
+
+ fprintf(stdout, "\n");
+ close(sock);
+ exit(0);
+} 
---
+++ server_tcp.c
@@ -0,0 +1,113 @@
+// Nombre : servidor
+// Proposito : Recibe un mensaje y lo envia de vuelta al
+ cliente
+// Autor : Edwin Carrasco (Adaptado de [1])
+// FCreacion : --
+// FModific. : 24/04/2009
+// Compilacion : gcc -o Servidor Servidor.c
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#define MAXPENDING 5 /* Nro maximo de conexiones */
+#define BUFFSIZE 32
+void AvisarError(char *mensaje)
+{
+ perror(mensaje);
+ exit(1);
+}
+void AtenderCliente(int sock)
+{
+// Variables
+ char buffer[BUFFSIZE];
+ int received = -1;
+ // Recibir mensaje
+if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0)
+ {
+ AvisarError("No se pudo recibir bytes iniciales del
+ cliente");
+ }
+// Envia bytes y verifica mas datos entrantes
+while (received > 0)
+ {
+ // Enviar de vuelta los datos recibidos
+ if (send(sock, buffer, received, 0) != received)
+ {
+ AvisarError("No se pudo enviar bytes al cliente");
+ }
+
+ // Verificar mas datos
+ if ((received = recv(sock, buffer, BUFFSIZE, 0)) < 0)
+ {
+ AvisarError("No se pudo recibir bytes adicionales del
+ cliente");
+ }
+ }
+ close(sock);
+}
+int main(int argc, char *argv[])
+{
+ int serversock;
+ int clientsock;
+ struct sockaddr_in echoserver;
+ struct sockaddr_in echoclient;
+
+ if (argc != 2)
+ {
+ fprintf(stderr, "USO: servidor <puerto>\n");
+ exit(1);
+ }
+
+ // Crear un socket TCP
+ if ((serversock = socket(PF_INET, SOCK_STREAM,
+ IPPROTO_TCP)) < 0)
+ {
+ AvisarError("No se pudo crear socket");
+ }
+
+ /* Construir la estructura sockaddr_in del servidor */
+ // Limpiar la estructura
+ memset(&echoserver, 0, sizeof(echoserver));
+ // Protocolo Internet/IP
+ echoserver.sin_family = AF_INET;
+ // Direccion de ingreso
+ echoserver.sin_addr.s_addr = htonl(INADDR_ANY);
+ // Puerto en que escucha el servidor
+ echoserver.sin_port = htons(atoi(argv[1]));
+ // Enlazar el socket del servidor
+
+ if (bind(serversock, (struct sockaddr *) &echoserver,
+ sizeof(echoserver)) < 0)
+ {
+ AvisarError("No se pudo enlazar el socket de
+ servidor");
+ }
+
+ // Escuchar en el socket del servidor
+ if (listen(serversock, MAXPENDING) < 0)
+ {
+ AvisarError("No se pudo escuchar en el socket del
+ servidor");
+ }
+
+ // Ejecutar hasta que se cancele
+ while (1)
+ {
+ unsigned int clientlen = sizeof(echoclient);
+
+ // Esperar conexion de cliente
+ if ((clientsock=accept(serversock,(struct sockaddr *)
+ &echoclient, &clientlen)) < 0 )
+ {
+ AvisarError("No se pudo aceptar conexion de
+ cliente");
+ }
+
+ fprintf(stdout, "Cliente conectado: %s\n",
+ inet_ntoa(echoclient.sin_addr));
+ AtenderCliente(clientsock);
+ }
+}
